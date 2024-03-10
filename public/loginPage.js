eecbbdad255bbc6f7480b22309b146698a3fa992
MaryL <marishka61.88@gmail.com>
2024-03-10 19:46:28 +0300

    первая часть задания готова

---
+++ public/loginPage.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const = new UserForm();
+
+userForm.loginFormCallback = (data) => {
+    console.log('data', data);
+
+    ApiConnector.login(data, (response) => {
+        console.log(response);
+
+        if(response) {
+            location.reload();  //обновить страницу
+        } else {
+            userForm.setLoginErrorMessage(message);
+        }
+    });
+}
+
+userForm.registerFormCallback = (data) => {
+    console.log('data', data);
+
+    ApiConnector.register(data, (response) => {
+        console.log(response);
+
+        if(response) {
+            location.reload();  
+        } else {
+            userForm.setRegisterErrorMessage(message);
+        }
+    });
+}
